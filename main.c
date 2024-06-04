48c6aa6c6cad883d7c8d657448e485de1b3e96b0
Svitol <97558303+GigioMagno@users.noreply.github.com>
2024-06-04 08:31:12 +0200

    Add files via upload
    Update main. Fixed bugs "reduced search"

--- main.c
+++ main.c
@@ -1,123 +1,224 @@
-/////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//              Vito Giacalone  (546646)                \\
-/////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    /////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    //              Vito Giacalone  (546646)                \\
+    /////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
-//////////////////////// Libraries \\\\\\\\\\\\\\\\\\\\\\\
+    //////////////////////// Libraries \\\\\\\\\\\\\\\\\\\\\\\
 
 #include <stdio.h>
-#include "bstree.h"
-#include "mpi.h"
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
 #include <limits.h>
+#include "posting_list.h"
 #include "enquiry.h"
+#include "bstree.h"
+#include <stdbool.h>
 
+#define NAME_MAX 255
 
-//////////////////////// MAIN \\\\\\\\\\\\\\\\\\\\\\\\\\\
+bool *printed_docs;
+
+char** distribute_paths(const char* base_path, const char* ext, int total_paths, int rank, int size, int* my_num_paths) {
+    int num_paths_per_proc = total_paths / size;
+    int remainder = total_paths % size;
+
+    *my_num_paths = (rank < remainder) ? num_paths_per_proc + 1 : num_paths_per_proc;
+
+    char** core_paths = (char**)malloc(*my_num_paths * sizeof(char*));
+    for (int i = 0; i < *my_num_paths; i++) {
+        core_paths[i] = (char*)malloc(NAME_MAX * sizeof(char));
+    }
+
+    if (rank == 0) {
+        char total_paths_arr[total_paths][NAME_MAX];
+
+        // Creazione dei path completi
+        for (int i = 0; i < total_paths; ++i) {
+            snprintf(total_paths_arr[i], NAME_MAX, "%s%d%s", base_path, i, ext);
+        }
+
+        int offset = 0;
+        for (int i = 0; i < size; i++) {
+            int send_count = (i < remainder) ? num_paths_per_proc + 1 : num_paths_per_proc;
+            if (i == 0) {
+                for (int j = 0; j < send_count; j++) {
+                    strcpy(core_paths[j], total_paths_arr[offset + j]);
+                }
+            } else {
+                // Corretto il modo di inviare i path
+                for (int j = 0; j < send_count; j++) {
+                    MPI_Send(total_paths_arr[offset + j], NAME_MAX, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+                }
+            }
+            offset += send_count;
+        }
+    } else {
+        for (int i = 0; i < *my_num_paths; i++) {
+            MPI_Recv(core_paths[i], NAME_MAX, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+    }
+
+    return core_paths;
+}
+
+void initialize_printed_docs(int size) {
+    printed_docs = (bool *)calloc(size, sizeof(bool));
+}
+
+void free_printed_docs() {
+    free(printed_docs);
+}
+
+void print_document(char **map, int doc_ID, int key, int rank) {
+    int index = (rank != 0) ? doc_ID % key : doc_ID - 1;
+    if (index < 0 || index >= key) return;
+
+    if (!printed_docs[index]) {
+        printf("%s\n", map[index]);
+        printed_docs[index] = true;
+    }
+}
+
 
 int main(int argc, char *argv[]) {
-
-    //////////////////////// MPI LAYER INIT \\\\\\\\\\\\\\\\\\\\\\\\\\\
-    // Initialize the MPI environment and obtain the rank and size of the processes
     int size, rank;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // printf("rank: %d argc %d\n", rank, argc); // Print the rank and number of arguments for debugging
+    char base_path_inverted_indexes[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/Preprocessing/InvertedIndex/inverted_idx_";
+    char base_path_sw[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/Preprocessing/Preprocess/stemwords_";
+    char base_path_dest_tree[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/output/bst_";                        
+    char base_path_map[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/Preprocessing/Documents/mapping_"; 
+    char ext[] = ".txt";
+    char writeout_tree[NAME_MAX];
 
-    //////////////////////// PATH-BUILDING FOR IO OPERATIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\
-    // Define base paths and extensions for various file operations
-    int words = 0;
-    char path_ix[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/Preprocessing/InvertedIndex/inverted_idx_"; // Base path for reading inverted index
-    char path_sw[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/Preprocessing/Preprocess/stemwords_";       // Base path for reading stemmed words
-    char path_dest_tree[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/output/bst_";                        // Base path for saving the BST
-    //char path_res[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/output/res_";                              // Base path for result files
-    char path_map[] = "/Users/svitol/Desktop/DCRBproject/binary_tree/Preprocessing/Documents/mapping_";         // Base path for document mapping
-    char ext[] = ".txt";                                                                                        // File extension for all paths
-    char complete_path_ix[NAME_MAX];                                                                            
-    char complete_path_sw[NAME_MAX];                                                                            
-    char complete_path_bst[NAME_MAX];                                                                           
-    //char complete_path_result[NAME_MAX];
-    char complete_path_map[NAME_MAX];                                                                         
-    sprintf(complete_path_ix, "%s%d%s", path_ix, rank, ext); // Complete path for inverted index reading
-    sprintf(complete_path_sw, "%s%d%s", path_sw, rank, ext); // Complete path for reading stemmed words
-    sprintf(complete_path_bst, "%s%d%s", path_dest_tree, rank, ext); // Complete path for BST storing
-    //sprintf(complete_path_result, "%s%d%s", path_res, rank, ext); // Complete path for result files
-    sprintf(complete_path_map, "%s%d%s", path_map, rank, ext); // Complete path for document mapping
+    int paths_num;
+    char** paths_rank_inverted_index = distribute_paths(base_path_inverted_indexes, ext, 12, rank, size, &paths_num);
+    char** paths_rank_stemwords = distribute_paths(base_path_sw, ext, 12, rank, size, &paths_num);
+    char** paths_rank_map = distribute_paths(base_path_map, ext, 12, rank, size, &paths_num);
 
-    //////////////////////// FILES OPENING \\\\\\\\\\\\\\\\\\\\\\\\\\\
-    // Open necessary files for reading and writing
-    FILE *fstem = fopen(complete_path_sw, "r");
-    FILE *f = fopen(complete_path_bst, "w");
+    node *root = NULL;
 
-    if (fstem == NULL || f == NULL) {
-        perror("Errore nell'apertura dei file"); // Print error if file opening fails
-        MPI_Finalize(); // Finalize MPI processes
-        return EXIT_FAILURE;
-    }
+    // Ogni processo stampa i path che ha ricevuto
+    //printf("Processo %d ha ricevuto %d path:\n", rank, paths_num);
+    // for (int i = 0; i < paths_num; i++) {
+    //     printf("%s\n", paths_rank_inverted_index[i]);
+    //     printf("%s\n", paths_rank_stemwords[i]);
+    //     printf("%s\n", paths_rank_map[i]);
+    // }
 
-    //////////////////////// BST BUILDING \\\\\\\\\\\\\\\\\\\\\\\\\\\
-    // Build the binary search tree (BST) from the stemmed words
-    node *root = NULL; // Root of the BST
-    char buf[SIZE]; // Temporary buffer for storing a row of the file
-    while (fgets(buf, sizeof(buf), fstem)) { // Read each stemmed word
-        size_t len_buf = strlen(buf); // Compute the length of the stemmed word
-        if (len_buf > 0 && buf[len_buf - 1] == '\n') { // Remove \n characters
-            buf[len_buf - 1] = '\0';
+    char buf[512]; 
+
+    for (int i = 0; i < paths_num; ++i) {
+        printf("Processing file: %s\n", paths_rank_stemwords[i]);
+        FILE *fstem = fopen(paths_rank_stemwords[i], "r");
+        if (fstem == NULL) {
+            printf("Null pointer exception\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
         }
-        safe_recursive_add_node(&root, create_node(buf)); // Add a node to the BST with the stemmed word
-    }
-    fclose(fstem); // Close the stemmed word file
 
-    node *node_set = build_bst(complete_path_ix, &words); // Create an array with all nodes to insert in the BST from the inverted index
-
-    for (int i = 0; i < words; ++i) { // For each node to insert
-        safe_recursive_add_node(&root, &node_set[i]); // Add the node to the BST
+        while (fgets(buf, sizeof(buf), fstem)) {
+            size_t len_buf = strlen(buf); 
+            if (len_buf > 0 && buf[len_buf - 1] == '\n') { 
+                buf[len_buf - 1] = '\0';
+            }
+            safe_recursive_add_node(&root, create_node(buf)); 
+        }
+        fclose(fstem);
     }
- 
-    // Write the BST to a file
-    safe_recursive_print_tree(root, f); // Print the BST to a file
-    fclose(f); // Close the file containing the new BST
-    
-    ////////////////////////// EXACT SEARCH \\\\\\\\\\\\\\\\\\\\\\\\\\\\
-    // Perform exact search on the BST
+
+    int words = 0;
+    for (int i = 0; i < paths_num; ++i) {
+        node *node_set = build_bst(paths_rank_inverted_index[i], &words);
+        for (int j = 0; j < words; ++j) {
+            safe_recursive_add_node(&root, &node_set[j]);
+        }
+    }
+
+    snprintf(writeout_tree, NAME_MAX, "%s%d%s", base_path_dest_tree, rank, ext);
+    FILE *ftree = fopen(writeout_tree, "w");
+    safe_recursive_print_tree(root, ftree);
+    fclose(ftree);
 
     int elements = 0;
     int key = 0;
-    char **idmap = map_ids(complete_path_map, &elements, &key); // Map document IDs
+    // for (int i = 0; i < paths_num; ++i)
+    // {
+    //     printf("kkkkkk:%s\n", paths_rank_map[i]);
+    // }
+    char **idmap = map_ids_from_paths(paths_rank_map, paths_num, &elements, &key);
+    if (idmap == NULL) {
+            printf("Error in creating ID map\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
 
+        initialize_printed_docs(elements);
 
-    lower_string(argv[1]); // Convert the search key to lowercase
-    node *result = get_node(root, argv[1]); // Get the node corresponding to the search key
-    int result_set_size = 0;
+        int result_set_size = 0;
+        int type_of_query;
 
-    int type_of_query;
+        if (rank == 0) {
+            printf("Choose the type of query:\n1 Disjunctive\n2 Conjunctive\n");
+            scanf("%d", &type_of_query);
+        }
+        
+        for (int i = 1; i < argc; ++i) {
+            lower_string(argv[i]);
+        }
+        
+        MPI_Bcast(&type_of_query, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if (rank == 0)
-    {
-        printf("Choose the type of query:\n1 Disjunctive\n2 Conjunctive\n");
-        scanf("%d", &type_of_query);
-    }
-    
-    MPI_Bcast(&type_of_query, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        switch (type_of_query) {
+            case 1:
+                disjunctive_query(root, idmap, key, &argv[1], argc-1, rank);
+                break;
+            case 2:
+                conjunctive_query(root, idmap, key, &argv[1], argc-1, rank);
+                break;
+            default:
+                printf("Invalid query type\n");
+                break;
+        }
 
-    switch(type_of_query) {
+        free_printed_docs();
 
-        case 1:
-            disjunctive_query(root, idmap, key, &argv[1], argc-1, rank);
-        case 2:
-            conjunctive_query(root, idmap, key, &argv[1], argc-1, rank);
-    }
-
-    // Execute different types of queries based on the search
-
-    // search_query(idmap, key, result, rank);
-    // 
-    
-
-    // Free the memory allocated for the tree
-    // safe_recursive_free_tree(root);
-
-    MPI_Finalize(); // Finalize MPI processes
-    return EXIT_SUCCESS;
+    MPI_Finalize();
+    return 0;
 }
+
+        // lower_string(argv[1]); // Convert the search key to lowercase
+        // node *result = get_node(root, argv[1]); // Get the node corresponding to the search key
+        // int result_set_size = 0;
+
+        // int type_of_query;
+
+        // if (rank == 0)
+        // {
+        //     printf("Choose the type of query:\n1 Disjunctive\n2 Conjunctive\n");
+        //     scanf("%d", &type_of_query);
+        // }
+        
+        // MPI_Bcast(&type_of_query, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        // switch(type_of_query) {
+
+        //     case 1:
+        //         disjunctive_query(root, idmap, key, &argv[1], argc-1, rank);
+        //     case 2:
+        //         conjunctive_query(root, idmap, key, &argv[1], argc-1, rank);
+        // }
+
+        // // Execute different types of queries based on the search
+
+        // // search_query(idmap, key, result, rank);
+        // // 
+        
+
+        // // Free the memory allocated for the tree
+        // // safe_recursive_free_tree(root);
+
+        // MPI_Finalize(); // Finalize MPI processes
+        // return EXIT_SUCCESS;
+    // //}
